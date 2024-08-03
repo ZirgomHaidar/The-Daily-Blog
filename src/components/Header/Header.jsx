7deb2cf2ad70807875aa966b63fe3e6f53dafef6
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-08-03 13:38:07 +0530

    Complete Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,28 @@ import blogicon from "../../assets/blogico.svg"
 
 function Header() {
   return (
-    <header>
-      <h1 className="text-3xl font-bold underline">
-        <span>
-          <img className="w-24" src={blogicon} alt="Blog" />
-        </span>
-        The Daily Blog
-      </h1>
+    <header className="flex items-center justify-between">
+      <span className="flex items-center gap-3">
+        <img className="w-12" src={blogicon} alt="Blog" />
+        <h1 className="text-4xl font-bold">The Daily Blog</h1>
+      </span>
+      <div className="post flex items-center gap-5">
+        <Results />
+        <SearchPosts />
+        <button className="border-2 border-purple-400 p-2 bg-purple-400">
+          Clear posts
+        </button>
+      </div>
     </header>
   )
 }
 
 export default Header
+
+function SearchPosts() {
+  return <input className="border-2 p-2" placeholder="Search posts..." />
+}
+
+function Results() {
+  return <p>🚀 X atomic posts found</p>
+}
